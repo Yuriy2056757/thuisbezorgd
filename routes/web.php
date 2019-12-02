90d4a26c3f918447d6b0f18fea2a02f5266bfb37
yuriy2056757 <2056757@talnet.nl>
2019-12-02 02:07:01 +0100

    Make home the default route

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,6 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/', 'HomeController@index')->name('home');
 
 Route::resource('restaurants', 'RestaurantController', ['except' => ['index']]);
