0ddca0d93f64aff7c6011ce3c5ba8473db6731ba
yuriy2056757 <2056757@talnet.nl>
2019-12-02 02:20:59 +0100

    Add restaurant search input

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,17 +4,13 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
+            <h1>Find a restaurant</h1>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+            <div class="input-group">
+                <input type="text" class="form-control" placeholder="Restaurants">
 
-                    You are logged in!
+                <div class="input-group-append">
+                    <span class="input-group-text">Search</span>
                 </div>
             </div>
         </div>
